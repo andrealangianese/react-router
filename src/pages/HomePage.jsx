e65009498a58dc05401b5d4513aa21302e3fe99b
andrealangianese <andre.langia.01@gmail.com>
2026-01-28 14:50:55 +0100

    feat: aggiungo sezione prodotti

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,8 @@ function HomePage() {
 
   return (
     <>
-      <h3>sarò homepage</h3>
+      <h3>Ciao bello, vieni a comprare!</h3>
+      <img src="https://c8.alamy.com/compit/2fxaf4k/foto-di-simpatica-simpatica-giovane-donna-divertente-faccia-funky-lingua-grimace-fuori-isolato-su-sfondo-di-colore-rosa-2fxaf4k.jpg" alt="" />
     </>
   )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,36 @@
+import { useState , useEffect} from "react"
+import axios from "axios"
+
 function Products() {
 
-  return (
-    <>
-      <h3>i nostri prodotti</h3>
-    </>
-  )
+    const endpoint = "https://fakestoreapi.com/products"
+
+    //var d stato per i prodotti
+    const [prodotti, setProdotti] = useState([])
+
+    //funzione con axios per recuperare i dati
+
+    function getProducts() {
+        axios.get(endpoint)
+            .then((resp) => setProdotti(resp.data))
+    }
+
+    //stampo solo al caricamento della pagina
+
+    useEffect(getProducts, [])
+    return (
+        <>
+            <h3>i nostri prodotti</h3>
+            <div>
+                {prodotti.map(prod => (
+                    <div key={prod.id}>
+                        <h3>{prod.title}</h3>
+                       <img src= {prod.image} alt={prod.title} width="100" />
+                    </div>
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default Products
