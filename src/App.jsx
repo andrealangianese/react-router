331f475f9f612029719229ddf4c76e897f331da9
andrealangianese <andre.langia.01@gmail.com>
2026-01-28 14:17:56 +0100

    feat: importo BrowserRouter, Routes, Route  e li metto in pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+//importo i componenti per la gestione delle rotte
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+//importo nuove pages 
+
+import HomePage from "./pages/HomePage"
+import AboutUs from "./pages/AboutUs"
+import Products from "./pages/Products"
 
 function App() {
 
   return (
     <>
       <h1>eccoti</h1>
+      <BrowserRouter>
+        <Routes>
+          {/* in questo caso mostrerà solo homepage + h1 che effettivamente è gia presente */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sudinoi" element={<AboutUs />} />
+          <Route path="/ourproducts" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
