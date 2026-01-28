a67f192b5b57a46d8fac5e499ca02a47815e55a3
andrealangianese <andre.langia.01@gmail.com>
2026-01-28 16:14:07 +0100

    feat: navbar dinamica con .map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,19 @@
 import { Link, NavLink } from "react-router-dom";
 
-export default function NavBar(){
+export default function NavBar() {
+
+    //generalizzo e raggruppo voci navbar
+
+    const links = [
+        { riferimento: "/", descrizioneEvento: "HomePage" },
+        { riferimento: "/su-di-noi", descrizioneEvento: "Chi siamoo" },
+        { riferimento: "/our-products", descrizioneEvento: "I nostri prodotti " }
+    ]
     return (
         <>
             <nav>
                 <ul>
-                    <li>
+                    {/* <li>
                         <NavLink to="/">HomePage </NavLink>
                     </li>
                     <li>
@@ -13,7 +21,17 @@ export default function NavBar(){
                     </li>
                     <li>
                         <NavLink to="/our-products">I nostri Prodotti </NavLink>
-                    </li>
+                    </li> */}
+
+                    {links.map(link => (
+                        <li key={link.id}>
+                            <NavLink to={link.riferimento}>
+                                
+                                {link.descrizioneEvento}
+                            </NavLink>
+                        </li>
+                    )
+                    )}
                 </ul>
             </nav>
         </>
