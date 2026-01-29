ac104a3f42c183aea915d03584ba0423d47b9175
andrealangianese <andre.langia.01@gmail.com>
2026-01-29 14:17:15 +0100

    feat: aggiungo link in Homepage che manda direttamente alla sez prodotti

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import NavBar from "../components/NavBar"
+import { Link } from "react-router-dom"
 
 function HomePage() {
 
@@ -6,6 +7,7 @@ function HomePage() {
         <>
             <NavBar />
             <h3>Ciao bello, vieni a comprare!</h3>
+            <button><Link to="/our-products">Vai ai prodotti</Link></button>
             <img src="https://c8.alamy.com/compit/2fxaf4k/foto-di-simpatica-simpatica-giovane-donna-divertente-faccia-funky-lingua-grimace-fuori-isolato-su-sfondo-di-colore-rosa-2fxaf4k.jpg" alt="" />
         </>
     )
