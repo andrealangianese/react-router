dbed98577fc76d4e98f2bc6e2d7cb04b690911c6
andrealangianese <andre.langia.01@gmail.com>
2026-01-29 14:44:03 +0100

    feat: aggiungo con useparams il dettaglio del singolo prodotto

--- src/pages/DetailProducts.jsx
+++ src/pages/DetailProducts.jsx
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom"
+
 export default function DetailProducts(){
+   
+    const { id } = useParams()
+   
     return(
 
-        <h3>Dettagli prodotto</h3>
-        
+        <h3>Dettagli prodotto {id}</h3>
     )
 }
 
