07ff75b652f20865c44f959cad048816a39b2ed0
andrealangianese <andre.langia.01@gmail.com>
2026-01-29 14:56:34 +0100

    feat: nuova chiamata API con aggiunta di id

--- src/pages/DetailProducts.jsx
+++ src/pages/DetailProducts.jsx
@@ -1,10 +1,23 @@
 import { useParams, Link } from "react-router-dom"
 
+// imp axios per chiamata
 
+import axios from "axios"
+
+import { useEffect } from "react"
+
+//endpoint per aggiungere i dettagli
+
+const endpointNew = "https://fakestoreapi.com/products"
 
 export default function DetailProducts() {
 
     const { id } = useParams()
+
+    useEffect(() => {   
+        axios.get(`${endpointNew}/${id}`)
+            .then((resp) => console.log(resp.data))
+    }, [])
 
     return (
         <>
